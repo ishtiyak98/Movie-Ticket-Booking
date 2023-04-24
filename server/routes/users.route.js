374bb7e4cd30f695f74dc44bb796482675eb6252
ishtiyak98 <rifatishtiyak@gmail.com>
2023-04-24 16:42:37 +0600

    login with JWT

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   try {
@@ -29,6 +30,40 @@ router.post("/register", async (req, res) => {
       message: error.message,
     });
   }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (!existingUser) {
+      return res.send({
+        success: false,
+        message: "user doesn't exist",
+      });
+    }
+    const validPass = await bcrypt.compare(
+      req.body.password,
+      existingUser.password
+    );
+
+    if (!validPass) {
+      return res.send({
+        success: false,
+        message: "invalid password",
+      });
+    } else {
+      const token = jwt.sign(
+        { userId: existingUser._id },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+      );
+      return res.send({
+        success: true,
+        message: "user logged in successfully",
+        data: token,
+      });
+    }
+  } catch (error) {}
 });
 
 module.exports = router;
